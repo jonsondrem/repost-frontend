baedf5c757fc6f78c3025d999b845342f7212235
jonsondrem <jonsondrem@gmail.com>
2020-02-18 22:05:47 +0100

    More general progress

--- repost/main.js
+++ repost/main.js
@@ -4,3 +4,17 @@ var app = new Vue({
         product: 'Test'
     }
 })
+
+var current_user = new Vue({
+    el: '#login',
+    data: {
+        user: ''
+    },
+    mounted () {
+        fetch('http://127.0.0.1:8000/api/users/me')
+            .then(response => response.json())
+            .then(json => {
+                this.user = json.username;
+            })
+    }
+})
