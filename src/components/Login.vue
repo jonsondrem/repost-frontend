9f309e1c28721a6f9a5b23c640c8fc11a28582bb
jonsondrem <jonsondrem@gmail.com>
2020-04-14 20:25:20 +0200

    removed auth.js

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,49 @@
 <template>
     <div class="login">
-        <form action="">
+        <form class="login_form">
             <label for="username">Username</label>
-            <input type="text" id="username" name="username">
+            <input type="text" id="username" name="username" v-model="username">
             <label for="password">Password</label>
-            <input type="text" id="password" name="password">
-            <input type="submit" value="Login">
+            <input type="text" id="password" name="password" v-model="password">
+            <input type="submit" value="Login" @click="handleSubmit">
         </form>
     </div>
 </template>
 
 <script>
+    import Vue from 'vue';
+
     export default {
-        name: "Login.vue"
+        name: "Login.vue",
+        data() {
+            return {
+                username: '',
+                password: ''
+            }
+        },
+        methods: {
+            handleSubmit(e) {
+                e.preventDefault()
+                if (this.password.length > 0) {
+                    Vue.http.post('http://127.0.0.1:8000/api/auth/token', {
+                        email: this.email,
+                        password: this.password
+                    })
+                        .then(response => {
+                            localStorage.setItem('user', JSON.stringify(response.data.user))
+                            localStorage.setItem('jwt', response.data.token)
+
+                            if (localStorage.getItem('jwt') != null) {
+                                this.$emit('loggedIn')
+                                this.$router.push('')
+                            }
+                        })
+                        .catch(function (error) {
+                            console.error(error.response);
+                        });
+                }
+            }
+        }
     }
 </script>
 
@@ -20,11 +51,11 @@
     .login {
         position: absolute;
         background-color: #2e2e2e;
-        width: 25%;
+        width: 250px;
         left: 50%;
         top: 200px;
         transform: translate(-50%, 0);
         color: white;
-        height: 400px;
+        height: 300px;
     }
 </style>
